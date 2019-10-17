d3321771676072131921e59d2b645115ed7af11a
RomanOt <vatokato@gmail.com>
2019-10-17 23:23:04 +0500

    lesson3 hw

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import './styles/index.scss'
+
+const priceButtons = [...document.querySelectorAll('.prices-btn')];
+priceButtons.forEach(button =>
+  button.addEventListener('click', e => alert(`Buy ${e.target.dataset.type}`))
+);
